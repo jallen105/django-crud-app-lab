c557275e9cc79df88434125f11ba155c4695f9a2
Jonathan <jonathanallen105@gmail.com>
2025-10-10 14:46:07 -0400

    added the url paths and basic responses for the home cards index and details pages.

--- my_app/views.py
+++ my_app/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return HttpResponse('Home page.')
+
+def details(request):
+    return HttpResponse('details page')
+
+def cards_index(request):
+    return HttpResponse('Cards index page')

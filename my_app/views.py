843c6a937a38eec7aba0a946e5504a3e1fb27eec
Jonathan <jonathanallen105@gmail.com>
2025-10-10 15:21:50 -0400

    added a card class and some sample data. updated the card index view to render the card index template

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+class Card:
+    def __init__(self, name, oracle_text, power, toughness):
+        self.name = name
+        self.oracle_text = oracle_text
+        self.power = power
+        self.toughness = toughness
+
+cards = [
+    Card('Llanwar Elf', 'tap add one mana', 1, 1),
+    Card('Ghalta', 'Trample', 12, 12),
+    Card('Monastery Swiftspear', 'Haste, Prowess', 1, 2),
+    Card('Avacyn, Angel of Hope', 'Flying, vigilance, indestructible', 8, 8)
+]
+
 def home(request):
     return render(request, 'home.html')
 
@@ -8,4 +22,4 @@ def details(request):
     return HttpResponse('details page')
 
 def cards_index(request):
-    return HttpResponse('Cards index page')
+    return render(request, 'cards/index.html', {'cards': cards})
